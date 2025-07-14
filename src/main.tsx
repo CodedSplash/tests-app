6aa1d9f4ac190b94d9ed8159dcb08631560a6f1b
Svyatoslav <svyatoslavwrk@gmail.com>
2025-07-14 20:21:34 +0300

    use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,43 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ColorModeScript } from '@chakra-ui/system'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import { Navigate, Route, HashRouter as Router, Routes } from 'react-router-dom'
 import theme, { config as themeConfig } from './theme'
 import { Layout } from './components/Layout'
 import { HomePage } from './pages/HomePage'
 import { TestPage } from './pages/TestPage'
 import { ResultPage } from './pages/ResultPage'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Navigate to={'/tests-app/'} replace />
-  },
-  {
-    path: '/tests-app/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: 'test/:testId',
-        element: <TestPage />,
-      },
-      {
-        path: 'result/:testId',
-        element: <ResultPage />,
-      },
-    ],
-  },
-])
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={themeConfig.initialColorMode} />
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/tests-app/" replace />} />
+          <Route path="/tests-app/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="test/:testId" element={<TestPage />} />
+            <Route path="result/:testId" element={<ResultPage />} />
+          </Route>
+        </Routes>
+      </Router>
     </ChakraProvider>
   </StrictMode>,
 )
